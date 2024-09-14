c128540e6cbe75bbedcc9eb3f770f7121809d3a9
Katie <kymlu.4@gmail.com>
2024-09-13 21:36:18 -0600

    Faves are broken but that's ok

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,28 @@
 import type { MarkdownInstance } from "astro";
+import { recipeIndex } from "./recipe-index";
+
+export const SITE_NAME: string = "eating-memories";
+export const PAGE_NAMES = {
+    HOME: "",
+    SITEMAP: "sitemap",
+    ABOUT: "about",
+    FAQ: "faq",
+    FAVES: "faves"
+}
+
+export const LOCAL_STORAGE_KEYS = {
+    FAVES: "faves"
+}
+
+export const LOCAL_STORAGE_SEPARATOR = ",";
+
+export function getPageLink(page?: string) {
+    return `/${SITE_NAME}${page ? `/${page}` : ""}`;
+}
+
+export function getPostLink(postId: number){
+    return `/${SITE_NAME}/posts/${recipeIndex[postId]}`;
+}
 
 export function dateSort (a: MarkdownInstance<Record<string, any>>, b: MarkdownInstance<Record<string, any>>){
     return Date.parse(b.frontmatter.pubDate) - Date.parse(a.frontmatter.pubDate);
