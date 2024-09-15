a5fc80f58fbde522361a333238c6b0556bc6cf18
Katie <kymlu.4@gmail.com>
2024-09-15 15:39:35 -0600

    add global file

---
+++ src/const.ts
@@ -0,0 +1,30 @@
+
+export const SITE_TITLE: string = "Recipe Box";
+export const SITE_SLUG: string = "eating-memories";
+export const PAGE_NAMES = {
+    HOME: "",
+    SITEMAP: "sitemap",
+    ABOUT: "about",
+    FAQ: "faq",
+    SAVED: "saved"
+}
+
+export const LOCAL_STORAGE_KEYS = {
+    SAVED: "saved"
+}
+
+export const ICONS = {
+    LOGO: "mingcute:fork-knife-line",
+    SAVED: "mingcute:bookmark-fill",
+    NOT_SAVED: "mingcute:bookmark-line",
+    EXPAND: "mingcute:down-line",
+    COLLAPSE: "mingcute:up-line",
+    PLUS: "mingcute:plus-fill",
+    MINUS: "mingcute:minimize-fill",
+    HAPPY: "mingcute:happy-line",
+    SAD: "mingcute:sad-line",
+    GITHUB: "mingcute:github-2-line",
+    CLOSE: "mingcute:close-line",
+    MENU: "mingcute:menu-line",
+    SUN: "mingcute:sun-line",
+}
