defdc8a70985593d5e9053c936105a035aca5452
Katie <kymlu.4@gmail.com>
2024-09-15 22:28:50 -0600

    Sorting fixes

--- src/utils.ts
+++ src/utils.ts
@@ -10,6 +10,17 @@ export function getPostLink(postId: string){
     return `/${SITE_SLUG}/posts/${recipeIndex[postId]}`;
 }
 
+export function titleSort(a: MarkdownInstance<Record<string, any>>, b: MarkdownInstance<Record<string, any>>){
+    let aTitle = (a.frontmatter.title ?? "" as string).toLowerCase();
+    let bTitle = (b.frontmatter.title ?? "" as string).toLowerCase();
+    if(bTitle < aTitle) {
+        return 1;
+    } else if (bTitle > aTitle) {
+        return -1;
+    } 
+    return 0;
+}
+
 export function dateSort (a: MarkdownInstance<Record<string, any>>, b: MarkdownInstance<Record<string, any>>){
     return Date.parse(b.frontmatter.pubDate) - Date.parse(a.frontmatter.pubDate);
 }
