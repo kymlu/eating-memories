4a955340af1deefa35a222753961cdae42426e35
Katie <kymlu.4@gmail.com>
2024-09-17 01:29:27 -0600

    Another nesting attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import tailwindcssNesting from 'tailwindcss/nesting'
 
 import icon from 'astro-icon';
 
@@ -9,6 +10,15 @@ import icon from 'astro-icon';
 export default defineConfig({
   site: 'https://kymlu.github.io',
   base: 'eating-memories',
+  vite: {
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcssNesting(),
+        ]
+      }
+    }
+  },
   integrations: [
     tailwind({
       nesting: true
